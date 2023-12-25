cb87bde1ab50fe80e7a9d24111c8e51216b5285f
laura.zab <laura.zablit@epfedu.fr>
2023-12-25 21:04:42 +0200

    feat: fin de la collection de pièces, passsage d'un niveau à un autre

---
+++ Assets/Scripts/LoadSpecificScene.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadSpecificScene : MonoBehaviour
+{
+
+    public string sceneName;
+    public Animator fadeSystem;
+
+    private void Awake()
+    {
+        fadeSystem = GameObject.FindGameObjectWithTag("FadeSystem").GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) 
+    {
+        if (collision.CompareTag("Player"))
+        {
+            StartCoroutine(LoadNextScene());
+        }
+    }
+
+    public IEnumerator LoadNextScene() 
+    {
+        fadeSystem.SetTrigger("FadeIn");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(sceneName);
+    }
+}
