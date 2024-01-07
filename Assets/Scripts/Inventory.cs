28d5d0536bd20ae3bcddd3a8a00f5afae724f95b
Julie Dlg <doligez.julie@gmail.com>
2024-01-07 03:36:00 +0100

    Inventaire et items 2

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -9,6 +9,9 @@ public class Inventory : MonoBehaviour
 
    public List<Item> content = new List<Item>();
    public int contentCurrentIndex = 0;
+   public Image itemImageUI;
+   public Text itemNameUI;
+   public Sprite emptyItemImage;
 
    public static Inventory instance;
 
@@ -23,32 +26,70 @@ public class Inventory : MonoBehaviour
         instance = this;
    }
 
+   private void Start()
+   {
+    UpdateIventoryUI();
+   }
+
    public void ConsumeItem()
    {
+        if(content.Count == 0)
+        {
+            return;
+        }
+
         Item currentItem = content[0];
         PlayerHealth.instance.HealPlayer(currentItem.hpGiven);
         PlayerMovement.instance.moveSpeed += currentItem.speedGiven;
         content.Remove(currentItem);
         GetNextItem();
+        UpdateIventoryUI();
    }
 
    public void GetNextItem()
    {
-    contentCurrentIndex++;
-    if(contentCurrentIndex > content.Count - 1)
-    {
-        contentCurrentIndex = 0;
-    }
+        if(content.Count == 0)
+        {
+            return;
+        }
+
+        contentCurrentIndex++;
+        if(contentCurrentIndex > content.Count - 1)
+        {
+            contentCurrentIndex = 0;
+        }
+        UpdateIventoryUI();
    }
 
    public void GetPreviousItem()
    {
-    contentCurrentIndex--;
-    if(contentCurrentIndex < 0)
-    {
-        contentCurrentIndex = content.Count - 1;
-    }
+        if(content.Count == 0)
+        {
+            return;
+        }
+
+        contentCurrentIndex--;
+        if(contentCurrentIndex < 0)
+        {
+            contentCurrentIndex = content.Count - 1;
+        }
+        UpdateIventoryUI();
    }
+
+   public void UpdateIventoryUI()
+   {
+        if(content.Count > 0)
+        {
+            itemImageUI.sprite = content[contentCurrentIndex].image;
+            itemNameUI.text = content[contentCurrentIndex].name;
+        }else
+        {
+            itemImageUI.sprite = emptyItemImage;
+            itemNameUI.text = "";
+        }
+        
+   }
+
 
    public void AddCoins(int count)
    {
