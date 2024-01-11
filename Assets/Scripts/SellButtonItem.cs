969e21024a9163954afb7ba345e16621cf5dbb6d
lucielong <lucie.long@epfedu.fr>
2024-01-11 22:17:40 +0100

    Ajout de la joconde et vente d'items

--- Assets/Scripts/SellButtonItem.cs
+++ Assets/Scripts/SellButtonItem.cs
@@ -11,17 +11,38 @@ public class SellButtonItem : MonoBehaviour
 
     public void BuyItem()
     {
-
         Inventory inventory = Inventory.instance;
 
-        if (inventory.coinsCount >= item.price)
+        if (item.price < 0)
         {
-            inventory.content.Add(item);
-            inventory.UpdateInventoryUI();
-            inventory.coinsCount -= item.price;
-            inventory.UpdateTextUI();
-
+            // Selling
+            if (inventory.coinsCount >= item.price)
+            {
+                inventory.content.Add(item);
+                inventory.UpdateInventoryUI();
+                inventory.coinsCount += item.price;
+                inventory.UpdateTextUI();
+            }
+            else
+            {
+                Debug.LogWarning("Not enough money to buy " + item.name);
+            }
+        }
+        else
+        {
+            // Buying
+            if (inventory.content.Contains(item))
+            {
+                inventory.content.Remove(item);
+                inventory.UpdateInventoryUI();
+                inventory.coinsCount += Mathf.Abs(item.price); 
+                inventory.UpdateTextUI();
+            }
+            else
+            {
+                Debug.LogWarning("No " + item.name + " in the inventory");
+            }
         }
     }
-
 }
+
